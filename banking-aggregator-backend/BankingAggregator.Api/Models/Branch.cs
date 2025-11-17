18e040c024fa797f9e3689b5c878663e69d67ce4
Aathityan Mariraj <aathityan.m@cspl.com>
2025-11-17 13:59:11 +0530

    "tables created"

--- banking-aggregator-backend/BankingAggregator.Api/Models/Branch.cs
+++ banking-aggregator-backend/BankingAggregator.Api/Models/Branch.cs
@@ -1,12 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace BankingAggregator.Api.Models { 
-    public class Branch { 
-        [Key] public Guid Id { get; set; } = Guid.NewGuid();
-        public Guid BankId { get; set; }
-        public string Name { get; set; } = ""; 
-        public string Code { get; set; } = ""; 
-        public string Address { get; set; } = ""; 
+public class Branch
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public Guid BankId { get; set; }
+    public string Name { get; set; } = "";
+    public string Code { get; set; } = "";
+    public string Address { get; set; } = "";
+
+    public Branch()
+    {
+        Id = Guid.NewGuid();
     }
 }
-
