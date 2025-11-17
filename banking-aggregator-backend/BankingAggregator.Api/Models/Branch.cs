704c48abc459e3eee2a46458515e44f8872106ec
Aathityan Mariraj <aathityan.m@cspl.com>
2025-11-17 13:57:30 +0530

    "commited and pushed"

--- banking-aggregator-backend/BankingAggregator.Api/Models/Branch.cs
+++ banking-aggregator-backend/BankingAggregator.Api/Models/Branch.cs
@@ -3,7 +3,7 @@
 namespace BankingAggregator.Api.Models { 
     public class Branch { 
         [Key] public Guid Id { get; set; } = Guid.NewGuid();
-        public Guid BankId { get; set; };
+        public Guid BankId { get; set; }
         public string Name { get; set; } = ""; 
         public string Code { get; set; } = ""; 
         public string Address { get; set; } = ""; 
